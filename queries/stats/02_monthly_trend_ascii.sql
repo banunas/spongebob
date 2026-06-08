38e888d053cd837ec7edcfcd51a51c495d4c4ad6
Seoa Lee <letterhill520@gmail.com>
2026-06-09 03:43:29 +0900

    feat: 통계 쿼리 수정 및 stats index.html 추이 컬럼 추가

--- queries/stats/02_monthly_trend_ascii.sql
+++ queries/stats/02_monthly_trend_ascii.sql
@@ -1,7 +1,11 @@
--- 담당: 팀원 2·4
+-- 담당: 서아
+-- 월별 협찬 건수 추이 (2026)
 -- 사용 기법: DATE_FORMAT + REPEAT('*', n)
-SELECT DATE_FORMAT(created_at, '%Y-%m') AS '월',
-COUNT(*) AS '협찬 건수'
+SELECT 
+    DATE_FORMAT(created_at, '%Y-%m') AS '월',
+    COUNT(*) AS '협찬 건수',
+    REPEAT('*', COUNT(*)) AS '추이'
 FROM sponsorship_request
+WHERE YEAR(created_at) = 2026
 GROUP BY DATE_FORMAT(created_at, '%Y-%m')
-ORDER BY '월' ASC;
+ORDER BY `월` ASC;
